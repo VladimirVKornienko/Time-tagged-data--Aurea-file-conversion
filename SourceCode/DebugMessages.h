57f12a3ae32fb4544538459449c27b977d0be07d
Vladimir V. Kornienko <VladimirVKornienko@yandex.ru>
2022-01-06 20:18:14 +0300

    Debug messages added, starting the debug with real data...

--- SourceCode/DebugMessages.h
+++ SourceCode/DebugMessages.h
@@ -4,33 +4,20 @@
 //	*	Debug flags (uncomment to use):		*	//
 //	*	*	*	*	*	*	*	*	*	*	*	//
 
-
-#ifndef AureaProcessorPart2OverflowDebuggingMessages
-//#define AureaProcessorPart2OverflowDebuggingMessages
-// << uncomment to use debugging on first 10 records. //
-#endif
-
-#ifndef AureaReverseChannelAndTimeData
-//#define AureaReverseChannelAndTimeData
-
-// << uncomment to write time tag first, and only then the channel data. //
-#endif
-
-
-#ifndef AureaNoHeaderToResultFile
-// #define AureaNoHeaderToResultFile
-// << uncomment for DEBUG MODE (ONLY). //
-#endif
-
 #ifndef stage4_Write_Overflow_Flags_ToFile
-// #define stage4_Write_Overflow_Flags_ToFile
+#define stage4_Write_Overflow_Flags_ToFile
+
 // (un)comment for writing (skipping) overflow markers when de-coding back from PTU format to Aurea format.
+
 // will be used in [stage3] also, if
 //				<stage3_DEBUG_re_create_files_with_text_tag_time_values> is defined.
+
+// 06.01.2022: works finely. //
+
 #endif
 
 #ifndef stage3_DEBUG_re_create_files_with_text_tag_time_values
-// #define stage3_DEBUG_re_create_files_with_text_tag_time_values
+#define stage3_DEBUG_re_create_files_with_text_tag_time_values
 
 // uncomment to create 2 add. files with tags and times, as seen in stage 3.
 // (Not from PTU file, as in Stage 4).
@@ -39,4 +26,33 @@
 // it has been proof-checked, it is working finely.
 
 // NB! works only for the last file in a batch (matter of simplicity, and enough for debugging).
+
+// 06.01.2022: works finely. //
+
+#endif
+
+#ifndef AureaProcessorPart2OverflowDebuggingMessages
+//#define AureaProcessorPart2OverflowDebuggingMessages
+// << uncomment to use debugging on first 10 records. //
+
+// 06.01.2022: obsolete. //
+
+#endif
+
+#ifndef AureaReverseChannelAndTimeData
+//#define AureaReverseChannelAndTimeData
+
+// << uncomment to write time tag first, and only then the channel data. //
+
+// 06.01.2022: not checked in the last version, may be faulty.. //
+
+#endif
+
+
+#ifndef AureaNoHeaderToResultFile
+// #define AureaNoHeaderToResultFile
+// << uncomment for DEBUG MODE (ONLY). //
+
+// 06.01.2022: o.k., not checked. //
+
 #endif
